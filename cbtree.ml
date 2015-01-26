082bf49bf349359495e8c5bcb220625f86fe2dfb
Justin Davis <jrcd83@gmail.com>
2015-01-26 11:54:36 -0500

    Tidy up. Condense lines a little bit.

--- cbtree.ml
+++ cbtree.ml
@@ -3,9 +3,9 @@ type t = Empty | Tree of node
 type bitstat = On | Off
 let empty = Empty
 
-(* Given two strings, find the byte index and the critical bit mask.
+(* Given two strings, find the byte index and the critical bit index.
    The result is packed into an integer, the lower 3 bits being the
-   bit index and the remaining 28 or 60 bits contain the byte index. *)
+   bit index, and the upper 28 or 60 bits containing the byte index. *)
 let cbcalc s t =
   let xor s t i = (Char.code s.[i]) lxor (Char.code t.[i]) in
   let rec cbcalc' s t len i =
@@ -23,35 +23,34 @@ let cbcalc s t =
       (* Pack byte number and critical bit index into a single int *)
       Some ((i lsl 3) lor (m - 1))
   in
-  if String.length s = String.length t then
-    cbcalc' s t (String.length s) 0
+  if String.length s = String.length t then cbcalc' s t (String.length s) 0
   else
     let len = min (String.length s) (String.length t) in
-    match cbcalc' s t len 0 with
-      None -> Some (len lsl 3) | Some _ as x -> x
+    match cbcalc' s t len 0 with None -> Some (len lsl 3) | Some _ as x -> x
 
-let cbtest key cb =
-  try let c = Char.code key.[cb lsr 3] in
+(* Test the critical bit specified by the packed cb integer in a string. *)
+let cbtest s cb =
+  try let c = Char.code s.[cb lsr 3] in
     match c land (1 lsl (cb land 7)) with 0 -> Off | _ -> On
   with Invalid_argument _ -> Off
 
 let mem k cbt =
-  let rec walk k = function
-    Leaf k' -> k = k'
+  let rec walk k = function Leaf k' -> k = k'
   | Branch (left, cb, right) ->
     walk k (match cbtest k cb with On -> right | Off -> left)
   in match cbt with Empty -> false | Tree n -> walk k n
 
 let graft k cb n =
-  match cbtest k cb with
-    On -> Branch (n, cb, Leaf k) | Off -> Branch (Leaf k, cb, n)
+  match cbtest k cb with On -> Branch (n, cb, Leaf k)
+  | Off -> Branch (Leaf k, cb, n)
 
 exception Critbit of int
 
 let rec prune k = function
-  | Leaf l ->
-    (match cbcalc k l with None -> failwith "key already exists"
-     | Some cb -> raise (Critbit cb))
+  | Leaf l -> begin
+      match cbcalc k l with None -> failwith "key already exists"
+      | Some cb -> raise (Critbit cb)
+    end
   | Branch (left, cb, right) ->
     let dir = cbtest k cb in
     try prune k (match dir with On -> right | Off -> left)
@@ -62,29 +61,24 @@ let rec prune k = function
       | Off -> Branch (graft k newcb left, cb, right)
 
 let add k cbt =
-  match cbt with
-    Empty -> Tree (Leaf k)
-  | Tree n -> try Tree (prune k n)
-    with Critbit cb -> Tree (graft k cb n)
+  match cbt with Empty -> Tree (Leaf k)
+  | Tree n -> try Tree (prune k n) with Critbit cb -> Tree (graft k cb n)
 
 exception Foundkey of string
 
 let remove k t =
   let rec walk k = function
-    Leaf l when k = l -> raise (Foundkey l)
-  | Leaf _ -> failwith "key not found"
+    Leaf l -> if k = l then raise (Foundkey l) else failwith "key not found"
   | Branch (left, cb, right) ->
     let dir = cbtest k cb in
     try match dir with On -> Branch (left, cb, walk k right)
-    | Off -> Branch (walk k left, cb, right)
+      | Off -> Branch (walk k left, cb, right)
     with Foundkey l -> match dir with On -> left | Off -> right
-  in match t with
-    Empty -> failwith "key not found"
+  in match t with Empty -> failwith "key not found"
   | Tree (Leaf l) -> if k = l then Empty else failwith "key not found"
   | Tree (b) -> Tree (walk k b)
 
-let iter ~f cbt =
-  let rec walk f = function
-    Leaf k -> f k | Branch (left, _, right) -> walk f left; walk f right
-  in match cbt with
-    Empty -> () | Tree n -> walk f n
+let iter ~f t =
+  let rec walk f = function Leaf k -> f k
+  | Branch (left, _, right) -> walk f left; walk f right
+  in match t with Empty -> () | Tree n -> walk f n
